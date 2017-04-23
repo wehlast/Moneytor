f6c5c8d9b1b3be3d6904e2f691b9002bc76d98b7
Mads Wehlast Hansen <wehlast@gmail.com>
2017-04-23 23:05:08 +0200

    Initial commit

---
+++ app/src/main/java/moneytor/moneytor/Income.java
@@ -0,0 +1,71 @@
+package moneytor.moneytor;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+public class Income extends Fragment {
+
+    EditText inty;
+    EditText innr;
+    Button submit_in;
+    ListView listView_in;
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> adapter;
+    int income = 0;
+
+
+    public Income() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View veiw = inflater.inflate(R.layout.fragment_income, container, false);
+
+        inty = (EditText) inty.findViewById(R.id.infield);
+        innr = (EditText) innr.findViewById(R.id.infieldnr);
+        submit_in = (Button) submit_in.findViewById(R.id.subincome);
+        arrayList = new ArrayList<String>();
+        ListView listView = (ListView) listView_in.findViewById(R.id.listveiw_in);
+
+        ArrayAdapter<String> listVeiwAdpter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, arrayList);
+        listView_in.setAdapter(adapter);
+
+        onBtnClick();
+
+        return veiw;
+    }
+
+    public void onBtnClick(){
+        submit_in.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String result = inty.getText().toString();
+                Integer num = Integer.valueOf(innr.getText().toString());
+                adapter.notifyDataSetChanged();
+                arrayList.add(result);
+                arrayList.add(num.toString());
+                income = income + num;
+            }
+        });
+    }
+}
---
+++ app/src/main/java/moneytor/moneytor/Summary.java
@@ -0,0 +1,34 @@
+package moneytor.moneytor;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Summary extends Fragment {
+
+    public Summary() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View veiw = inflater.inflate(R.layout.fragment_summary, container, false);
+
+
+
+        return veiw;
+    }
+
+}
